ddd58599dc279e8c7b2627f8e7b62e51d59898da
hanbyul0103 <all007b@naver.com>
2025-10-24 17:31:39 +0900

    feat: command register & etc..

--- src/index.js
+++ src/index.js
@@ -7,7 +7,12 @@ import {
 import dotenv from "dotenv";
 dotenv.config();
 
-import { initializeDataFiles } from "./data/jsonHelper.js";
+// 라이브러리
+
+// 외부 함수들
+import * as jsonHelper from "./data/jsonHelper.js";
+import * as commandRegister from "./utils/commandRegister.js";
+import * as loadCommands from "./utils/loadCommands.js";
 
 const client = new Client({
     intents: [
@@ -18,14 +23,39 @@ const client = new Client({
     ]
 });
 
+let commands;
+
 const thisYear = () => {
     return new Date().getFullYear();
 }
 
-client.on("ready", async () => {
+client.once("ready", async () => {
     console.log("[!] ready");
 
-    initializeDataFiles(thisYear());
-})
+    // 명령어 세팅
+    await commandRegister.registCommands();
+
+    commands = await loadCommands.getCommands();
+
+    // 폴더 및 파일 세팅
+    jsonHelper.initializeDataFiles(thisYear());
+});
+
+client.on("interactionCreate", async (interaction) => {
+    // 채팅이 아니라면 리턴
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = commands.get(interaction.commandName);
+
+    if (!command) return;
+
+    try {
+        await command.callback(client, interaction);
+    } catch (error) {
+        console.log(`명령어 실행 중 예상치 못한 오류가 발생했습니다.`, error);
+
+        await interaction.reply({ content: `명령어 실행 중 예상치 못한 오류가 발생했습니다.`, ephemeral: true });
+    }
+});
 
 client.login(process.env.TOKEN);
